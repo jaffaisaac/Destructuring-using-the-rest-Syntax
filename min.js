dcd3bd02c049d3f600053a9ee76af93316cd45ba
jaffa Isaac <jaffaisaac4@gmail.com>
2023-02-23 15:34:48 +0300

    Add files via upload

---
+++ min.js
@@ -0,0 +1,15 @@
+var num1 ,num2 ,num3 , rest ;
+var  a,b,c,d ,other;
+
+
+[num1 ,num2 ,num3 ,...rest] =[1,2,3,4,5,6,7,8,9,10];
+
+[a,b,...other] =['jafa','Ben','joy','Peace','Marry','May ', 'peter']
+
+console.log(num1);
+console.log(num2);
+console.log(rest);
+
+
+console.log(a ,b);
+console.log(other);
